0b9391bced4b87938fd252ab8411c69c0f65ddd9
Wiltort <wiltort21@gmail.com>
2023-10-24 20:46:18 +0500

    задача Б исправлена

--- task_B_Birzha.py
+++ task_B_Birzha.py
@@ -6,19 +6,21 @@ def play():
     with open('input.txt') as input:
         n = int(input.readline()) #принимаем массив
         P = [int(a) for a in input.readline().split()]
-    S=0
-    left = sorted(P)[0:n//2]
+    S = 0
+    print(P)
     while P:
-        max_index, max_value = max(enumerate(P), key=lambda pair: pair[1])
-        if max_index == 0:
+        if P[0] >= P[1]:
             P.pop(0)#если максимум на первом месте - удаляем
+            n -= 1
         else:
-            for i in range(max_index-1,-1,-1):
-                if P[i] in left:
-                    S += max_value-P[i]
-                    P.pop(max_index)
-                    P.pop(i)
-                    break        
+            for i in range(2,n):
+                if P[i-1]>P[i]:
+                    S += P[i-1]-P[0]
+                    P = P[i:]
+                    n -= i
+                    break
+            S += P[n-1]-P[0]
+            P = []
     return S
 
 if __name__=="__main__":
